dedbb1c7c94c32e24489eb804b3d03877df45477
Roman Patsiaichuk <roman.shady@yandex.ru>
2023-05-03 01:32:33 +0300

    feat: wrapping state into object

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-import { rerenderEntireTree } from './render'
 import reportWebVitals from './reportWebVitals';
-import state from './redux/state'
+import store from './redux/state'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-rerenderEntireTree(state);
+let root = ReactDOM.createRoot(document.getElementById('root'))
+
+export let rerenderEntireTree = (state) => {
+  root.render(
+    <React.StrictMode>
+      <App state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)} />
+    </React.StrictMode>
+  );
+}
+
+
+rerenderEntireTree(store.getState())
 reportWebVitals();
+store.subscribe(rerenderEntireTree);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,61 +1,74 @@
-import { rerenderEntireTree } from '../render.js'
 import { v4 as uuid } from 'uuid';
-let state = {
 
-  profilePage: {
-    posts: [
-      { id: "1", message: "'Hi, how are you", likeCount: "0" },
-      { id: "2", message: "'Hi, how are you", likesCount: '23' },
-      { id: "3", message: "It's, my first post", likesCount: "0" },
-      { id: "4", message: "It's, my first post", likesCount: "0" },
-      { id: "5", message: "It's, my first post", likesCount: "0" },
-      { id: "6", message: "It's, my first post", likesCount: "0" },
-    ],
-    newPostText: 'it-kamasutra.com'
+let store = {
+  _state: {
+
+    profilePage: {
+      posts: [
+        { id: "1", message: "'Hi, how are you", likeCount: "0" },
+        { id: "2", message: "'Hi, how are you", likesCount: '23' },
+        { id: "3", message: "It's, my first post", likesCount: "0" },
+        { id: "4", message: "It's, my first post", likesCount: "0" },
+        { id: "5", message: "It's, my first post", likesCount: "0" },
+        { id: "6", message: "It's, my first post", likesCount: "0" },
+      ],
+      newPostText: 'it-kamasutra.com'
+    },
+
+    dialogsPage: {
+      messages: [
+        { id: "6", message: "Hi" },
+        { id: "7", message: "Hello" },
+        { id: "8", message: "How are you?" },
+      ],
+      dialogs: [
+        {
+          id: "1",
+          name: 'Roman'
+        },
+        {
+          id: "2",
+          name: 'Sveta'
+        },
+        {
+          id: "3",
+          name: 'Andrey'
+        },
+        {
+          id: "4",
+          name: 'Seny'
+        },
+        {
+          id: "5",
+          name: 'Sacha'
+        }
+      ],
+    },
   },
 
-  dialogsPage: {
-    messages: [
-      { id: "6", message: "Hi" },
-      { id: "7", message: "Hello" },
-      { id: "8", message: "How are you?" },
-    ],
-    dialogs: [
-      {
-        id: "1",
-        name: 'Roman'
-      },
-      {
-        id: "2",
-        name: 'Sveta'
-      },
-      {
-        id: "3",
-        name: 'Andrey'
-      },
-      {
-        id: "4",
-        name: 'Seny'
-      },
-      {
-        id: "5",
-        name: 'Sacha'
-      }
-    ],
+  getState() {
+    return this._state
   },
+
+  _callSubscriber() {
+    console.log("state changed!");
+  },
+
+  addPost() {
+    let newPost = { id: uuid(), message: this._state.profilePage.newPostText, likesCount: '0' }
+    this._state.profilePage.posts.push(newPost)
+    this._state.profilePage.newPostText = ''
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText
+    this._callSubscriber(this._state);
+  },
+
+  subscribe(observer) {
+    this._callSubscriber = observer
+  }
 }
 
-export let addPost = () => {
-  let newPost = { id: uuid(), message: state.profilePage.newPostText, likesCount: '0' }
-  state.profilePage.posts.push(newPost)
-  state.profilePage.newPostText = ''
-  rerenderEntireTree(state);
-}
 
-
-export let updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText
-  rerenderEntireTree(state);
-}
-
-export default state;
+export default store;
