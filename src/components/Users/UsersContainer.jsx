607ce78e6843871ec4efcec45c777fe380c1d7a2
Roman Patsiaichuk <roman.shady@yandex.ru>
2023-10-18 01:02:04 +0300

    feat: added DAL level

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,35 +1,29 @@
 import { connect } from 'react-redux'
 import { unfollow, follow, setUsers, setCurrentPage, setTotalUsersCount, toggleIsFetching } from '../../redux/users-reducer'
-import axios from 'axios'
 import React from 'react'
 import Users from './Users'
 import Preloader from '../common/Preloader/Preloader'
+import { usersAPI } from '../../api/api'
 class UsersAPIComponent extends React.Component {
 
   componentDidMount() {
     this.props.toggleIsFetching(true)
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {
-      withCredentials: true
-    })
-      .then((response) => {
-        this.props.toggleIsFetching(false)
-        this.props.setUsers(response.data.items)
-        // this.props.setTotalUsersCount(response.data.totalCount)
-        this.props.setTotalUsersCount(100)
+    usersAPI.getUsers(this.props.currentPage, this.props.pageSize).then((data) => {
+      this.props.toggleIsFetching(false)
+      this.props.setUsers(data.items)
+      // this.props.setTotalUsersCount(data.totalCount)
+      this.props.setTotalUsersCount(100)
 
-      })
+    })
   }
 
-  onPageChanged = (item) => {
-    this.props.setCurrentPage(item)
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber)
     this.props.toggleIsFetching(true)
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${item}&count=${this.props.pageSize}`, {
-      withCredentials: true
+    usersAPI.getUsers(pageNumber, this.props.pageSize).then((data) => {
+      this.props.toggleIsFetching(false)
+      this.props.setUsers(data.items)
     })
-      .then((response) => {
-        this.props.toggleIsFetching(false)
-        this.props.setUsers(response.data.items)
-      })
 
   }
 
